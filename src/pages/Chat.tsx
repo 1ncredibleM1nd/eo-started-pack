c19a42098ca9d005a38b0e5329444c61a5b9bc5d
AntonLyshko <antonlishko@gmail.com>
2021-01-29 17:41:28 +0300

    News

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { inject, observer } from 'mobx-react';
-import { Layout } from 'antd';
+import { Row, Col, Layout } from 'antd';
 import { ChatLayout, InfoLayout, ContactsLayout } from '@layouts'
 import IStores, { IAppStore } from '@stores/interface';
 import '@styles/index.scss'
@@ -17,13 +17,27 @@ const App = inject((stores: IStores) => ({ appStore: stores.appStore }))(
 
         // const collapsed_info = appStore.info_tab
 
-        // const { Sider } = Layout;
+        const { Sider } = Layout;
 
         return (
             <Layout hasSider={true} className='chat_page'>
-                <ContactsLayout />
-                <ChatLayout />
-                <InfoLayout />
+                <Row>
+                    <Col xs={0} sm={10} md={10} lg={8} xl={8}>
+
+                        <Sider>
+                            <ContactsLayout />
+                        </Sider>
+                    </Col>
+                    <Col xs={24} sm={14} md={14} lg={11} xl={11}>
+                        <ChatLayout />
+                    </Col>
+                    <Col xs={0} sm={0} md={0} lg={6} xl={6}>
+                        <InfoLayout />
+                    </Col>
+                </Row>
+
+
+
             </Layout>
         );
     }));
