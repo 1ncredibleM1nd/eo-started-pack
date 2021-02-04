52a38c9df464effb711f3a0818752775ae4d4fd6
Avet Khanoyan <avokhanoyanfs@gmail.com>
2021-02-04 18:46:28 +0400

    added set interval to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react';
-import { inject, observer } from 'mobx-react';
-import { Layout } from 'antd';
-import IStores, { IAppStore } from '@stores/interface';
+import React, {useEffect} from 'react';
+import {inject, observer} from 'mobx-react';
+import {Layout} from 'antd';
+import IStores, {IAppStore} from '@stores/interface';
 import Chats from '@pages/Chat'
 import '@styles/index.scss'
 
@@ -9,19 +9,23 @@ type IProps = {
     appStore?: IAppStore
 }
 
-const App = inject((stores: IStores) => ({ appStore: stores.appStore }))(
+const App = inject((stores: IStores) => ({appStore: stores.appStore}))(
     observer((props: IProps) => {
 
-        const { appStore } = props;
+        const {appStore} = props;
+
 
         useEffect(() => {
             async function init() {
                 try {
-                    await appStore.initialization();
+                    setInterval(async () => {
+                        await appStore.initialization();
+                    }, 1000)
                 } catch (e) {
                     throw new Error(e);
                 }
             }
+
             init();
         }, []);
         return (
@@ -29,8 +33,8 @@ const App = inject((stores: IStores) => ({ appStore: stores.appStore }))(
                 <Layout className="site-layout">
                     <div className="chats-tab-open h-100">
                         <div className={"main-layout h-100"}>
-                            <Chats />
-                            {/* <NavBarLayout /> */}
+                            <Chats/>
+                            {/*<NavBarLayout /> */}
                         </div>
                     </div>
                 </Layout>
