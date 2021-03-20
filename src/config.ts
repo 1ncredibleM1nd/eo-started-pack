02f31f9a6fe0a523e365ab3464b0d73227de3f52
Dmitry Artemov <dima.16.artemov@gmail.com>
2021-03-20 20:02:23 +0300

    PROD-726: - BASE_AUTH_URL in CONFIG

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export default class CONFIG {
-    static BASE_API_URL = "https://backend.chat.dev.prodamus.pro",
+    static BASE_API_URL = "https://backend.chat.dev.prodamus.pro"
     static BASE_AUTH_URL = "https://account.dev.prodamus.ru"
 }
 
