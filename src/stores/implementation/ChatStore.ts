9caf1bffce6bce22cc5d2cf5937560dce0bb1ab6
Dmitry Derenko <12042002gh>
2021-04-23 18:10:44 +0300

    PROD-1203: - группировка по user.id

--- src/stores/implementation/ChatStore.ts
+++ src/stores/implementation/ChatStore.ts
@@ -331,10 +331,11 @@ export class ChatStore implements IChatStore {
 			if (
 				previousMessage.date === message.current.date &&
 				previousMessage.income === message.current.income &&
-				previousMessage.entity.type === message.current.entity.type &&
-				previousMessage.user.id === user.id
+				previousMessage.entity.type === message.current.entity.type
 			) {
-				combineWithPrevious = true
+				if (previousMessage.user && user && !!previousMessage.user == !!user) {
+					combineWithPrevious = true
+				}
 			}
 
 			if (previousMessage.date !== message.current.date) {
