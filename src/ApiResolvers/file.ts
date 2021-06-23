16560cc5bdeed9d02ece335f9318341fea60ef2f
dzhirma <menscrem@gmail.com>
2021-06-23 12:15:00 +0300

    fileApi: improve error handle

--- src/ApiResolvers/file.ts
+++ src/ApiResolvers/file.ts
@@ -2,29 +2,25 @@ import { DOWNLOAD } from "actions/axios";
 import { notification } from "antd";
 
 export async function download(path: string, filename: string) {
-  const response = await DOWNLOAD.post(path, null, { responseType: "blob" });
-
-  if (response.data.type === "application/json") {
-    const data = JSON.parse(await response.data.text());
-
-    if (data?.error) {
-      notification.error({
-        placement: "topRight",
-        message: data?.data?.error_message,
-      });
-
-      return;
+  try {
+    const response = await DOWNLOAD.get(path, { responseType: "blob" });
+    console.log(response);
+    const url = URL.createObjectURL(new Blob([response.data]));
+    const link = document.body.appendChild(document.createElement("a"));
+    link.href = url;
+    link.download = filename;
+    link.click();
+    URL.revokeObjectURL(url);
+  } catch (error) {
+    console.error(error);
+    let error_message = "Не удалось запгрузить файл";
+    if (error?.response?.data) {
+      const result = JSON.parse(await error?.response?.data.text());
+      error_message = result?.data?.error_message;
     }
+    notification.error({
+      placement: "topRight",
+      message: error_message,
+    });
   }
-
-  const url = URL.createObjectURL(
-    new Blob([response.data], {
-      type: "application/octet-stream",
-    })
-  );
-  const link = document.body.appendChild(document.createElement("a"));
-  link.href = url;
-  link.download = filename;
-  link.click();
-  URL.revokeObjectURL(url);
 }
