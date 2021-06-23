883b68e88cfd20b699819079d111886e3fa9cef8
dzhirma <menscrem@gmail.com>
2021-06-23 11:49:01 +0300

    fileApi: improve error handle and add support blob

--- src/ApiResolvers/file.ts
+++ src/ApiResolvers/file.ts
@@ -1,23 +1,30 @@
 import { DOWNLOAD } from "actions/axios";
 import { notification } from "antd";
 
-export async function download(url: string, filename: string) {
-  const response = await DOWNLOAD.post(url);
-  if (response.data?.error) {
-    notification.error({
-      placement: "topRight",
-      message: response.data?.data?.error_message,
-    });
-  } else {
-    const url = URL.createObjectURL(
-      new Blob([response.data], {
-        type: "application/octet-stream",
-      })
-    );
-    const link = document.body.appendChild(document.createElement("a"));
-    link.href = url;
-    link.download = filename;
-    link.click();
-    URL.revokeObjectURL(url);
+export async function download(path: string, filename: string) {
+  const response = await DOWNLOAD.post(path, null, { responseType: "blob" });
+
+  if (response.data.type === "application/json") {
+    const data = JSON.parse(await response.data.text());
+
+    if (data?.error) {
+      notification.error({
+        placement: "topRight",
+        message: data?.data?.error_message,
+      });
+
+      return;
+    }
   }
+
+  const url = URL.createObjectURL(
+    new Blob([response.data], {
+      type: "application/octet-stream",
+    })
+  );
+  const link = document.body.appendChild(document.createElement("a"));
+  link.href = url;
+  link.download = filename;
+  link.click();
+  URL.revokeObjectURL(url);
 }
