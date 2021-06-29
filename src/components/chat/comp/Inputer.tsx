4e58e60409b2d8851e9194fc5b0d38122e614099
dzhirma <menscrem@gmail.com>
2021-06-29 14:22:47 +0300

    PROD-2212: make social button as icon for Inputer

--- src/components/chat/comp/Inputer.tsx
+++ src/components/chat/comp/Inputer.tsx
@@ -317,26 +317,27 @@ const Inputer = inject((stores: IStores) => ({
             )}
           </div>
 
-          {/* Button Social */}
+          {/* Button Social.  */}
           <div className="inputer_btn">
-            <Popover
-              visible={switcher === "1social"}
-              content={<SocialMenu selectSocial={selectSocial} />}
-              trigger="click"
-            >
-              <Button
-                disabled={!!chatError}
-                onClick={() => setSwitcher("social")}
-                className="transparent not-allowed"
-              >
-                <Icon
-                  className="icon_l"
-                  name={`social_media_${
-                    currentChat.activeSocial ? currentChat.activeSocial : ""
-                  }`}
-                />
-              </Button>
-            </Popover>
+            {/* TODO: temporarily disable, enable after meeting task */}
+            {/*<Popover*/}
+            {/*  visible={switcher === "social"}*/}
+            {/*  content={<SocialMenu selectSocial={selectSocial} />}*/}
+            {/*  trigger="click"*/}
+            {/*>*/}
+            {/*  <Button*/}
+            {/*    disabled={!!chatError}*/}
+            {/*    onClick={() => setSwitcher("social")}*/}
+            {/*    className="transparent not-allowed"*/}
+            {/*  >*/}
+            <Icon
+              className="icon_l"
+              name={`social_media_${
+                currentChat.activeSocial ? currentChat.activeSocial : ""
+              }`}
+            />
+            {/*</Button>*/}
+            {/*</Popover>*/}
           </div>
         </div>
 
