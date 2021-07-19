b7e08965dfe503e2e5e5c9d16636b5a992c86c2e
Dmitry Derenko <12042002gh>
2021-07-19 21:36:19 +0300

    PROD-2472 вложение "ссылка"

---
+++ src/components/chat/comp/MessageAttachmentLink.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { observer } from "mobx-react";
+import { LoadingOutlined } from "@ant-design/icons";
+import type { TMessageAttachment } from "@/types/message";
+
+type TProps = { attachment: TMessageAttachment };
+
+export const MessageAttachmentLink = observer(({ attachment }: TProps) => {
+  return (
+    <div>
+      <div className="msg-content-file">
+        <div className="document-preview">
+          {attachment.data ? (
+            <img src={attachment.data.preview} alt="" />
+          ) : (
+            <LoadingOutlined />
+          )}
+        </div>
+        <div className="file-title-container">
+          <a
+            href={attachment.url}
+            target={"_blank"}
+            className={"file-title"}
+            rel="noreferrer"
+          >
+            {attachment.title}
+          </a>
+          <span />
+        </div>
+      </div>
+    </div>
+  );
+});
