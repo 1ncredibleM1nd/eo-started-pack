0fc0a5878ceb9aab295b7aeae3c4ef812be2ad8b
dzhirma <menscrem@gmail.com>
2021-08-25 13:35:59 +0300

    fix codestyle

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ module.exports = (env, { mode }) => {
       stats: "minimal",
       historyApiFallback: true,
       watchOptions: { aggregateTimeout: 300, poll: 1000 },
+      disableHostCheck: true,
     },
     resolve: {
       alias: {
