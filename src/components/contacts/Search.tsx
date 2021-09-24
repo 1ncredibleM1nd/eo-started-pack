d58abaafca131c7ffa912c32fbe2b6d9ad61d35a
dzhirma <menscrem@gmail.com>
2021-09-24 08:46:00 +0300

    Revert "PROD-3392: enable search input"
    This reverts commit d2d91f67493e1c72497a40669b8d559cd2566734.

--- src/components/contacts/Search.tsx
+++ src/components/contacts/Search.tsx
@@ -1,16 +1,19 @@
-import "./Search.scss";
+import { Fragment } from "react";
 import { observer } from "mobx-react-lite";
 import { useStore } from "@/stores";
-import { Collapse, Button } from "antd";
+
+import { Input, Collapse, Button } from "antd";
+import "./Search.scss";
+
 import { FilterTags } from "@/components/Filter/FilterTags";
 import { FilterSchools } from "@/components/Filter/FilterSchools";
 import { FilterChannels } from "@/components/Filter/FilterChannels";
 import { useHistory } from "react-router-dom";
 import { Icon } from "@/ui/Icon/Icon";
-import { SearchInput } from "./SearchInput";
 
 const Search = observer(() => {
-  const { contactStore, schoolsStore, tagsStore, sidebarStore } = useStore();
+  const { contactStore, appStore, schoolsStore, tagsStore, sidebarStore } =
+    useStore();
   const history = useHistory();
   const filterSwitch = contactStore.filterSwitch;
 
@@ -33,34 +36,52 @@ const Search = observer(() => {
     contactStore.refetch();
   }
 
+  const { Panel } = Collapse;
+  const { Search } = Input;
+
   return (
-    <div className="contact_header">
-      <div className="search">
-        <div className="search-filter">
-          <Button
-            disabled={!contactStore.isLoaded}
-            onClick={() => contactStore.toggleFilterSwitch()}
-            className="transparent"
-          >
-            {filterSwitch ? (
-              <Icon name={"icon_filter_close"} fill="#a3a3a3" />
-            ) : (
-              <Icon name={"icon_filter"} fill="#a3a3a3" />
-            )}
-          </Button>
+    <Fragment>
+      <div className="contact_header">
+        <div className="search">
+          <div className="search-filter">
+            <Button
+              disabled={!contactStore.isLoaded}
+              onClick={() => contactStore.toggleFilterSwitch()}
+              className="transparent"
+            >
+              {filterSwitch ? (
+                <Icon name={"icon_filter_close"} fill="#a3a3a3" />
+              ) : (
+                <Icon name={"icon_filter"} fill="#a3a3a3" />
+              )}
+            </Button>
+          </div>
+
+          {/* TODO: temporary hide by PROD-2058 */}
+          {/*<div className="search-input">*/}
+          {/*  <Search*/}
+          {/*    disabled*/}
+          {/*    placeholder="Поиск..."*/}
+          {/*    value={searchText}*/}
+          {/*    onChange={(e) => onChange(e.target.value)}*/}
+          {/*    enterButton*/}
+          {/*  />*/}
+          {/*</div>*/}
         </div>
 
-        <SearchInput />
+        <Collapse
+          bordered={false}
+          accordion
+          activeKey={filterSwitch ? "1" : ""}
+        >
+          <Panel header="" key="1">
+            <FilterTags onChangeTags={onChangeTags} />
+            <FilterSchools onChangeSchool={onChangeSchool} />
+            <FilterChannels onChangeSocial={onChangeSocial} />
+          </Panel>
+        </Collapse>
       </div>
-
-      <Collapse bordered={false} accordion activeKey={filterSwitch ? "1" : ""}>
-        <Collapse.Panel header="" key="1">
-          <FilterTags onChangeTags={onChangeTags} />
-          <FilterSchools onChangeSchool={onChangeSchool} />
-          <FilterChannels onChangeSocial={onChangeSocial} />
-        </Collapse.Panel>
-      </Collapse>
-    </div>
+    </Fragment>
   );
 });
 
