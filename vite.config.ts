f92386580fd13f6015fbe245980a89679c01870b
dzhirma <menscrem@gmail.com>
2021-10-07 18:00:02 +0300

    reduce build size

--- vite.config.ts
+++ vite.config.ts
@@ -34,6 +34,11 @@ export default defineConfig({
           libraryDirectory: "es",
           style: "css",
         },
+        {
+          libraryName: "lodash",
+          libraryDirectory: "",
+          camel2DashComponentName: false,
+        },
       ],
     }),
   ],
